5525b7fc7b43a713484a1d2cbca448b3a8df7a9b
Nilanjan Mukherjee <nilanjan1110@gmail.com>
2022-02-22 21:45:36 -0600

    initial commit

---
+++ src/test/java/com/nil/hello/HelloWorldServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nil.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
